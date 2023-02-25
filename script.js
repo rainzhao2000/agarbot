b8e59f12e1bc2bc6dfa09bf6ae4bd89fc627e3aa
Rain Zhao <rainzhao2000@gmail.com>
2023-02-25 10:33:14 -0500

    workers

--- script.js
+++ script.js
@@ -1,60 +1,25 @@
-import puppeteer from 'puppeteer';
+import { WorkerPool } from './worker-pool.js';
+import * as os from 'os';
 
 const args = {
-    botName: "",
+    concurrency: "",
     partyCode: ""
 };
 
-async function delay(ms) {
-    await new Promise((res) => {
-        setTimeout(() => res(), ms);
-    });
-    return true;
-}
-
-function clearInput(inputSelector) {
-    document.querySelector(inputSelector).value = "";
-}
-
-async function main() {
-    const config = {
-        headless: true
-    };
-    const browser = await puppeteer.launch(config);
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    await page.goto('https://agar.io/');
-    const connectionSelector = 'div#blocker>span.connecting-text';
-    await page.waitForSelector(connectionSelector, { timeout: 60000 });
-    await page.waitForFunction(delay, {}, 5000);
-    const joinSelector = 'button.party-join';
-    await page.waitForSelector(joinSelector);
-    await page.click(joinSelector);
-    const inputSelector = 'input.party-token';
-    await page.waitForSelector(inputSelector);
-    await page.evaluate(clearInput, inputSelector);
-    await page.type(inputSelector, args.partyCode);
-    await page.click(joinSelector);
-    await page.waitForFunction(delay, {}, 1000);
-    const nameSelector = 'input#nick';
-    await page.waitForSelector(nameSelector);
-    await page.evaluate(clearInput, nameSelector);
-    await page.type(nameSelector, args.botName);
-    await page.waitForFunction(delay, {}, 1000);
-    const playSelector = 'button.party-play';
-    await page.waitForSelector(playSelector);
-    await page.click(playSelector);
-    await page.mouse.move(10, 10);
-    await page.mouse.move(20, 20);
-    const statsSelector = 'div#stats';
-    await page.waitForSelector(statsSelector, { visible: true, timeout: 120000 });
-    await browser.close();
-}
-
-if (process.argv.length != Object.keys(args).length + 2) {
-    console.error(`Usage: node ${process.argv[1]} <botName> <partyCode>`)
-} else {
-    args.botName = process.argv[2];
-    args.partyCode = process.argv[3];
-    main();
-}
+(async () => {
+    if (process.argv.length != Object.keys(args).length + 2) {
+        console.error(`Usage: node ${process.argv[1]} <concurrency> <partyCode>`)
+    } else {
+        args.concurrency = process.argv[2];
+        args.partyCode = process.argv[3];
+        const pool = new WorkerPool(os.cpus().length);
+        let finished = 0;
+        for (let i = 1; i <= args.concurrency; ++i) {
+            pool.runTask({ botName: `M${i}`, partyCode: args.partyCode }, (err, res) => {
+                if (err) console.error(err);
+                else console.log(res);
+                if (++finished === args.concurrency) pool.close();
+            });
+        }
+    }
+})();
---
+++ worker-thread.js
@@ -0,0 +1,57 @@
+import { parentPort } from 'worker_threads';
+import puppeteer from 'puppeteer';
+
+parentPort.on('message', async (args) => {
+    try {
+        await exec(args);
+        parentPort.postMessage('success');
+    } catch (e) {
+        parentPort.postMessage(e);
+    }
+});
+
+async function delay(ms) {
+    await new Promise((res) => {
+        setTimeout(() => res(), ms);
+    });
+    return true;
+}
+
+function clearInput(inputSelector) {
+    document.querySelector(inputSelector).value = "";
+}
+
+async function exec({ botName, partyCode }) {
+    const config = {
+        headless: true
+    };
+    const browser = await puppeteer.launch(config);
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto('https://agar.io/');
+    // const connectionSelector = 'div#blocker>span.connecting-text';
+    // await page.waitForSelector(connectionSelector, { timeout: 60000 });
+    await page.waitForFunction(delay, {}, 5000);
+    const joinSelector = 'button.party-join';
+    await page.waitForSelector(joinSelector);
+    await page.click(joinSelector);
+    const inputSelector = 'input.party-token';
+    await page.waitForSelector(inputSelector);
+    await page.evaluate(clearInput, inputSelector);
+    await page.type(inputSelector, partyCode);
+    await page.click(joinSelector);
+    await page.waitForFunction(delay, {}, 1000);
+    const nameSelector = 'input#nick';
+    await page.waitForSelector(nameSelector);
+    await page.evaluate(clearInput, nameSelector);
+    await page.type(nameSelector, botName);
+    await page.waitForFunction(delay, {}, 1000);
+    const playSelector = 'button.party-play';
+    await page.waitForSelector(playSelector);
+    await page.click(playSelector);
+    await page.mouse.move(10, 10);
+    await page.mouse.move(20, 20);
+    const statsSelector = 'div#stats';
+    await page.waitForSelector(statsSelector, { visible: true, timeout: 60000 });
+    await browser.close();
+}
