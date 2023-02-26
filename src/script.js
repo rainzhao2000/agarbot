242ff1a4c7023f00616eb86ebf737f3f8702ea44
Rain Zhao <rainzhao2000@gmail.com>
2023-02-25 19:02:21 -0500

    rename

--- src/script.js
+++ src/script.js
@@ -3,27 +3,27 @@ import * as os from 'os';
 import puppeteer from 'puppeteer';
 
 const args = {
-    concurrency: "",
+    numBots: "",
     partyCode: ""
 };
 
 (async () => {
     if (process.argv.length != Object.keys(args).length + 2) {
-        console.error(`Usage: node ${process.argv[1]} <concurrency> <partyCode>`)
+        console.error(`Usage: node ${process.argv[1]} <numBots> <partyCode>`)
     } else {
-        args.concurrency = process.argv[2];
+        args.numBots = process.argv[2];
         args.partyCode = process.argv[3];
         const pool = new WorkerPool(os.cpus().length);
         const browser = await puppeteer.launch();
         let finished = 0;
-        for (let i = 1; i <= args.concurrency; ++i) {
+        for (let i = 1; i <= args.numBots; ++i) {
             pool.runTask({
                 browserWSEndpoint: browser.wsEndpoint(),
                 botName: `Diver ${i}`,
                 partyCode: args.partyCode
             }, async (err, res) => {
                 console.log(i, err, res);
-                if (++finished === args.concurrency) {
+                if (++finished === args.numBots) {
                     await browser.close();
                     pool.close();
                 }
